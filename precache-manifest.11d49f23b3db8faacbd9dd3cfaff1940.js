444c346c08f1e3bdacc9749767ce448047472597
BQ <qian.brian@gmail.com>
2019-04-17 15:02:36 -0700

    Updates

---
+++ precache-manifest.11d49f23b3db8faacbd9dd3cfaff1940.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  },
+  {
+    "revision": "186a72a3c4cb7a97d176",
+    "url": "/portfolio-react/static/js/main.1eb13f16.chunk.js"
+  },
+  {
+    "revision": "65a5fa58c0efca3be81c",
+    "url": "/portfolio-react/static/js/2.f96f56a8.chunk.js"
+  },
+  {
+    "revision": "f692068c6ae868c52d6032690f66888b",
+    "url": "/portfolio-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-react/precache-manifest.41e01ff9f2f4c68a056a0f7d18da8d1f.js"
+  "/portfolio-react/precache-manifest.11d49f23b3db8faacbd9dd3cfaff1940.js"
 );
 
 workbox.clientsClaim();
