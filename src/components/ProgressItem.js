62aa46d9ac235f7334b64c825afa96542d94d380
BQ <qian.brian@gmail.com>
2019-04-17 14:57:00 -0700

    overlay on resources page working

--- src/components/ProgressItem.js
+++ src/components/ProgressItem.js
@@ -6,22 +6,56 @@ const Container = styled.div`
   width: 100%;
 `;
 
+const ImgContainer = styled.div`
+  height: 200px;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Image = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-color: teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${ImgContainer}:hover & {
+    opacity: 0.2;
+  }
+`;
+
+const Overlay = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: -100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Markazi Text";
+  background-color: #00000065;
+  color: white;
+  transition: all 250ms;
+  ${ImgContainer}:hover & {
+    transform: translateY(100%);
+    z-index: 3;
+  }
+`;
+
 class ProgressItem extends Component {
-  handleHover = () => {
-    console.log("hello");
-  };
   render() {
-    const style = {
-      width: "100%",
-      height: "150px",
-      backgroundColor: "black",
-      border: "3px solid white",
-    };
     return (
       <Container>
-        {this.props.title}
-        <div style={style} onMouseEnter={this.handleHover} />
-        {this.props.description}
+        <h2>{this.props.title}</h2>
+        <ImgContainer>
+          <Overlay>{this.props.description}</Overlay>
+          <Image>I'm the Image</Image>
+        </ImgContainer>
       </Container>
     );
   }
