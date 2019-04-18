10e9cb5387af4924168e2e4b48817ec89e1e6fc7
BQ <qian.brian@gmail.com>
2019-04-18 00:34:03 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-react/precache-manifest.df2760d4f075a7da481cf287278d83b3.js"
+  "/portfolio-react/precache-manifest.cefbdd751377c3f1c2b212199f86410b.js"
 );
 
 workbox.clientsClaim();
